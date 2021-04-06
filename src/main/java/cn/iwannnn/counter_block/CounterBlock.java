19b33d4047e81c4ccde65fd25f19502c4d1cd59c
iwannnn <Zhangyaowen11@icloud.com>
2021-04-06 18:59:44 +0800

    modify

---
+++ src/main/java/cn/iwannnn/counter_block/CounterBlock.java
@@ -0,0 +1,21 @@
+// package cn.iwannnn.counter_block;
+
+// import net.minecraft.block.Block;
+// import net.minecraft.block.BlockState;
+// import net.minecraft.block.Blocks;
+// import net.minecraft.block.material.Material;
+// import net.minecraft.entity.player.PlayerEntity;
+// import net.minecraft.tileentity.TileEntity;
+// import net.minecraft.util.ActionResultType;
+// import net.minecraft.util.Hand;
+// import net.minecraft.util.math.BlockPos;
+// import net.minecraft.util.math.BlockRayTraceResult;
+// import net.minecraft.util.text.TranslationTextComponent;
+// import net.minecraft.world.IBlockReader;
+// import net.minecraft.world.World;
+
+// public class CounterBlock extends Block {
+//     public CounterBlock() {
+//         super(Properties.of(Material.STONE).strength(5));
+//     }
+// }
